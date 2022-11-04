15278321862e4353cea2fcb83dfade18e43e3f99
Shinobirun <adrian.latorre@gmail.com>
2022-11-04 18:43:07 -0300

    headerSinDrop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FotoComponent } from './components/foto/foto.component';
+import { IconosSocialesComponent } from './components/iconos-sociales/iconos-sociales.component';
+import { DropdownComponent } from './components/dropdown/dropdown.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FotoComponent,
+    IconosSocialesComponent,
+    DropdownComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/iconos-sociales/iconos-sociales.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { IconosSocialesComponent } from './iconos-sociales.component';
+
+describe('IconosSocialesComponent', () => {
+  let component: IconosSocialesComponent;
+  let fixture: ComponentFixture<IconosSocialesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ IconosSocialesComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(IconosSocialesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
