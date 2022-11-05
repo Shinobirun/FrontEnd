96a7b11afc2ddc326f4b03d53d36fcf795d17dea
Shinobirun <adrian.latorre@gmail.com>
2022-11-05 18:37:54 -0300

    maquetadoInicialCompleto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -8,6 +9,13 @@ import { FotoComponent } from './components/foto/foto.component';
 import { IconosSocialesComponent } from './components/iconos-sociales/iconos-sociales.component';
 import { DropdownComponent } from './components/dropdown/dropdown.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { TrabajosComponent } from './components/trabajos/trabajos.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -16,11 +24,17 @@ import { BannerComponent } from './components/banner/banner.component';
     FotoComponent,
     IconosSocialesComponent,
     DropdownComponent,
-    BannerComponent
+    BannerComponent,
+    AcercaDeComponent,
+    TrabajosComponent,
+    ExperienciaComponent,
+    ProyectosComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
